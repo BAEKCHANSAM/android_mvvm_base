54680ee457f1575af58b48ea26bac9c62f7377eb
chansam <ckstoa100@naver.com>
2024-02-22 18:35:47 +0900

    base activity add (incomplete)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+//    id("com.android.application")
+//    id("kotlin-android")
+    id("androidx.navigation.safeargs.kotlin")
+    id("kotlin-kapt")
 
 }
 
@@ -34,10 +38,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-
-    buildFeatures {
-        dataBinding = true
-    }
+        buildFeatures {
+            dataBinding = true
+        }
 
 }
 
@@ -52,24 +55,25 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     //app
-    implementation ("androidx.appcompat:appcompat:1.6.1")
-    implementation ("androidx.cardview:cardview:1.0.0")
-    implementation ("com.google.android.material:material:1.11.0")
-    implementation ("androidx.recyclerview:recyclerview:1.3.2")
-    implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation ("androidx.annotation:annotation:1.7.1")
-    implementation ("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.annotation:annotation:1.7.1")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
 
     //aac
-    implementation ("androidx.lifecycle:lifecycle-extensions:2.2.0")
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
-    implementation ("androidx.navigation:navigation-fragment-ktx:2.7.7")
-    implementation( "androidx.navigation:navigation-ui-ktx:2.7.7")
-    implementation ("androidx.viewpager2:viewpager2:1.0.0")
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
 
     //kotlin
-    implementation ("org.jetbrains.kotlin:kotlin-stdlib:1.9.22")
-    implementation ("androidx.fragment:fragment-ktx:1.6.2")
-    implementation ("androidx.core:core-ktx:1.12.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.22")
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
+    implementation("androidx.core:core-ktx:1.12.0")
+}
 
 }
